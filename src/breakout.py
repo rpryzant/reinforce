11019ee98c8b494e2a6f370aea2cde46f38dcbd2
rapigan <rapigan@gmail.com>
2016-10-17 13:14:15 -0700

    starting to flesh out bot control abstractions

--- src/breakout.py
+++ src/breakout.py
@@ -3,6 +3,8 @@ Breakout
 """
 import sys
 import pygame
+import abc
+import math
 
 SCREEN_SIZE   = 640,480
 
@@ -85,30 +87,28 @@ class Breakout(object):
         for brick in self.bricks:
             pygame.draw.rect(self.screen, BRICK_COLOR, brick)
         
-    def check_input(self):
+    def check_input(self, input):
         boost = 5 if self.boost_time > 0 else 0
 
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_LEFT]:
+        if 'L' in input:
             self.paddle.left -= (7 + boost)
             if self.paddle.left < 0:
                 self.paddle.left = 0
 
-        if keys[pygame.K_RIGHT]:
+        if 'R' in input:
             self.paddle.left += (7 + boost)
             if self.paddle.left > MAX_PADDLE_X:
                 self.paddle.left = MAX_PADDLE_X
 
-        if keys[pygame.K_b] and self.boosts_remaining > 0 and self.boost_time == 0:
+        if 'B' in input and self.boosts_remaining > 0 and self.boost_time == 0:
             self.boosts_remaining -= 1
             self.boost_time += 25
 
-        if keys[pygame.K_SPACE] and self.state == STATE_BALL_IN_PADDLE:
+        if 'sp' in input and self.state == STATE_BALL_IN_PADDLE:
             self.ball_vel = [5,5]
             self.speed_multiplyer = 1.0
             self.state = STATE_PLAYING
-        elif keys[pygame.K_RETURN] and (self.state == STATE_GAME_OVER or self.state == STATE_WON):
+        elif 'ret' in input and (self.state == STATE_GAME_OVER or self.state == STATE_WON):
             self.init_game()
 
     def move_ball(self):
@@ -167,7 +167,29 @@ class Breakout(object):
             y = ((SCREEN_SIZE[1] - size[1]) / 2) + y_ofs
             self.screen.blit(font_surface, (x,y))
         
-            
+
+    @abc.abstractmethod
+    def run(self):
+        return
+
+
+class HumanControlledBreakout(Breakout):
+    """ TODO - move input recognizing logic, and run here """
+    def __init__(self):
+        super(HumanControlledBreakout, self).__init__()
+
+    def _get_input(self):
+        keys = pygame.key.get_pressed()
+        input = []
+        input += ['L'] if keys[pygame.K_LEFT] else []
+        input += ['R'] if keys[pygame.K_RIGHT] else []
+        input += ['B'] if keys[pygame.K_b] else []
+        input += ['sp'] if keys[pygame.K_SPACE] else []
+        input += ['ret'] if keys[pygame.K_RETURN] else []
+
+        return input
+        
+
     def run(self):
         while 1:            
             for event in pygame.event.get():
@@ -176,7 +198,7 @@ class Breakout(object):
 
             self.clock.tick(50)
             self.screen.fill(BLACK)
-            self.check_input()
+            self.check_input(self._get_input())
 
             if self.state == STATE_PLAYING:
                 self.move_ball()
@@ -207,20 +229,102 @@ class Breakout(object):
             pygame.display.flip()
 
 
-
-class HumanControlledBreakout(Breakout):
-    """ TODO - move input recognizing logic, and run here """
-    def __init__(self):
-        super(HumanControlledBreakout, self).__init__()
-
-
-
 class BotControlledBreakout(Breakout):
     """ TODO - create state vectors, don't have run draw stuff, give state vectors to learning agent (in another file""" 
-    def __init__(self):
+    def __init__(self, agent, display):
+        self.agent = agent
+        self.display = display
         super(BotControlledBreakout, self).__init__()
+
+    def run(self):
+        while 1:            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit
+
+            if self.display:
+                self.clock.tick(50)
+                self.screen.fill(BLACK)
+
+                self._discretizeLocation(self.paddle.x, self.paddle.y)
+
+
+#  TODO - THINK MORE ABOUT THIS...DISCRETIZE SPEED?
+#            state_vector = {
+#                "paddle_pos": self._discretizeLocation(self.paddle.x, self.paddle.y),
+#                "ball_pos": self._discretizeLocation(self.ball.x, self.ball.y)
+#                "ball_angle": self._discretizeAngle(self.ball_vel)
+#                "boosts_remaining": self.boosts_left
+#                }
+
+            if self.state == STATE_PLAYING:
+                self.move_ball()
+                self.handle_collisions()
+            elif self.state == STATE_BALL_IN_PADDLE:
+                self.ball.left = self.paddle.left + self.paddle.width / 2
+                self.ball.top  = self.paddle.top - self.ball.height
+                self.show_message("PRESS SPACE TO LAUNCH THE BALL")
+                self.show_message("PRESS B TO BOOST", 0, 30)
+            elif self.state == STATE_GAME_OVER:
+                self.show_message("GAME OVER. PRESS ENTER TO PLAY AGAIN")
+            elif self.state == STATE_WON:
+                self.show_message("YOU WON! PRESS ENTER TO PLAY AGAIN")
+
+            self.boost_time = max(self.boost_time - 1, 0)
+                
+            if self.display:
+                self.draw_bricks()
+
+                # Draw paddle
+                if self.boost_time > 0:
+                    pygame.draw.rect(self.screen, PINK, self.paddle)                
+                else:
+                    pygame.draw.rect(self.screen, BLUE, self.paddle)
+
+                # Draw ball
+                pygame.draw.circle(self.screen, WHITE, (self.ball.left + BALL_RADIUS, self.ball.top + BALL_RADIUS), BALL_RADIUS)
+
+                self.show_stats()
+
+                pygame.display.flip()
+    
+
+    def _discretizeLocation(self, x, y):
+        """ 
+        converts continuous coordinates in R^2 to discrete location measurement 
+
+        does so by converting game board to grid of 20x20 pixel squares, then
+          gives the index of the square that (x, y) is in
+        """
+        entries_in_row = SCREEN_SIZE[0] / 20
+        x_grid = x / 10
+        y_grid = y / 10
+        return x_grid + y_grid * (SCREEN_SIZE[0] / 20)
+
+    
+    # TODO - put many of these in utils file
+    def dotProduct(self, a, b):
+        return sum(x * y for (x, y) in zip(a, b))
+
+    def _magnitude(self, a):
+        """ magnitude of vector """
+        return math.sqrt(self.dotProduct(a, a))
+
+
+    def _angle_between(self, a, b):
+        """ angle between two vectors"""
+
+        return math.degrees(math.acos(self.dotProduct(a, b) / (self._magnitude(a) * self._magnitude(b))))
+
+    def _discretizeAngle(self, vec):
+        """ 
+        buckets the continuous angle of a vector into one of 16 discrete angle categories
+        """
+        return int(self._angle_between([1,0], vec) / 16)
+
 
 
 
 if __name__ == "__main__":
-    HumanControlledBreakout().run()
+#    HumanControlledBreakout().run()
+    BotControlledBreakout(None, True).run()
