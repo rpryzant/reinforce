687d05164fa438141332b5f5dc85946af5a56e54
rapigan <rapigan@gmail.com>
2016-10-16 17:22:19 -0700

    renamed bricka, added boosts

--- breakout.py
+++ breakout.py
@@ -1,7 +1,5 @@
 """
 Breakout
- Adapted from Leonel Machava's implementation
-
 """
 import sys
 import pygame
@@ -27,6 +25,7 @@ PADDLE_Y = SCREEN_SIZE[1] - PADDLE_HEIGHT - 10
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 BLUE  = (0,0,255)
+PINK = (255,105,180)
 BRICK_COLOR = (200,200,0)
 
 # State constants
@@ -36,14 +35,14 @@ STATE_WON = 2
 STATE_GAME_OVER = 3
 
 
-class Bricka:
+class Breakout:
 
     def __init__(self):
         pygame.init()
         
 
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption("bricka (a breakout clone by codeNtronix.com)")
+        pygame.display.set_caption("Breakout!!")
         
         self.clock = pygame.time.Clock()
 
@@ -59,6 +58,8 @@ class Bricka:
         self.lives = 3
         self.score = 0
         self.num_hits = 0
+        self.boosts_remaining = 3
+        self.boost_time = 0
         self.speed_multiplyer = 1.0
         self.state = STATE_BALL_IN_PADDLE
 
@@ -85,17 +86,23 @@ class Bricka:
             pygame.draw.rect(self.screen, BRICK_COLOR, brick)
         
     def check_input(self):
+        boost = 5 if self.boost_time > 0 else 0
+
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_LEFT]:
-            self.paddle.left -= 7
+            self.paddle.left -= (7 + boost)
             if self.paddle.left < 0:
                 self.paddle.left = 0
 
         if keys[pygame.K_RIGHT]:
-            self.paddle.left += 7
+            self.paddle.left += (7 + boost)
             if self.paddle.left > MAX_PADDLE_X:
                 self.paddle.left = MAX_PADDLE_X
+
+        if keys[pygame.K_b] and self.boosts_remaining > 0 and self.boost_time == 0:
+            self.boosts_remaining -= 1
+            self.boost_time += 25
 
         if keys[pygame.K_SPACE] and self.state == STATE_BALL_IN_PADDLE:
             self.ball_vel = [5,5]
@@ -149,15 +156,15 @@ class Bricka:
 
     def show_stats(self):
         if self.font:
-            font_surface = self.font.render("SCORE: " + str(self.score) + " LIVES: " + str(self.lives), False, WHITE)
+            font_surface = self.font.render("SCORE: " + str(self.score) + " LIVES: " + str(self.lives) + " BOOSTS: " + str(self.boosts_remaining), False, WHITE)
             self.screen.blit(font_surface, (205,5))
 
-    def show_message(self,message):
+    def show_message(self,message, x_ofs = 0, y_ofs = 0):
         if self.font:
             size = self.font.size(message)
             font_surface = self.font.render(message,False, WHITE)
-            x = (SCREEN_SIZE[0] - size[0]) / 2
-            y = (SCREEN_SIZE[1] - size[1]) / 2
+            x = ((SCREEN_SIZE[0] - size[0]) / 2) + x_ofs
+            y = ((SCREEN_SIZE[1] - size[1]) / 2) + y_ofs
             self.screen.blit(font_surface, (x,y))
         
             
@@ -178,15 +185,19 @@ class Bricka:
                 self.ball.left = self.paddle.left + self.paddle.width / 2
                 self.ball.top  = self.paddle.top - self.ball.height
                 self.show_message("PRESS SPACE TO LAUNCH THE BALL")
+                self.show_message("PRESS B TO BOOST", 0, 30)
             elif self.state == STATE_GAME_OVER:
                 self.show_message("GAME OVER. PRESS ENTER TO PLAY AGAIN")
             elif self.state == STATE_WON:
                 self.show_message("YOU WON! PRESS ENTER TO PLAY AGAIN")
                 
             self.draw_bricks()
-
+            self.boost_time = max(self.boost_time - 1, 0)
             # Draw paddle
-            pygame.draw.rect(self.screen, BLUE, self.paddle)
+            if self.boost_time > 0:
+                pygame.draw.rect(self.screen, PINK, self.paddle)                
+            else:
+                pygame.draw.rect(self.screen, BLUE, self.paddle)
 
             # Draw ball
             pygame.draw.circle(self.screen, WHITE, (self.ball.left + BALL_RADIUS, self.ball.top + BALL_RADIUS), BALL_RADIUS)
@@ -196,4 +207,4 @@ class Bricka:
             pygame.display.flip()
 
 if __name__ == "__main__":
-    Bricka().run()
+    Breakout().run()
