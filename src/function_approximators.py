68b2fb1e6a67134c261d6fba4965fc4fbda359dc
rapigan <rapigan@gmail.com>
2016-11-09 12:20:22 -0800

    function approximators added

---
+++ src/function_approximators.py
@@ -0,0 +1,59 @@
+"""File containing all the fancy schmancy function approximators"""
+from collections import defaultdict
+import utils
+from copy import deepcopy
+import random
+
+################################# FEATURE EXTRACTORS ###################################
+
+
+
+
+class FunctionApproximator(object):
+    def __init__(self):
+        self.weights = defaultdict(float)
+        self.gamma = None
+        return
+
+    def set_gamma(self, g):
+        self.gamma = g
+
+    def set_weights(self, w):
+        self.weights = w
+
+    def get_weights(self):
+        return deepcopy(self.weights)
+
+    def getQ(self, state, action):
+        """forward pass of model"""
+        pass
+
+    def incorporate_feedback(self, prev_state, prev_action, reward, state, opt_action):
+        """adjust weights"""
+        pass
+
+
+class LinearFunctionApproximator(FunctionApproximator):
+    # TODO - NEED INTERACTION TERMS!!!
+    def __init__(self, feature_extractor):
+        super(LinearFunctionApproximator, self).__init__()
+        self.feature_extractor = feature_extractor
+        return
+
+    def getQ(self, state, action):
+        features = self.feature_extractor.get_features(state, action)
+
+        score = 0
+        for f, v in features.items():
+            score += self.weights[f] * v
+        return score
+
+    def incorporate_feedback(self, prev_state, prev_action, reward, state, opt_action, step_size):
+        features = self.feature_extractor.get_features(prev_state, prev_action)
+
+        target = reward + self.gamma * self.getQ(state, opt_action)
+
+        prediction = self.getQ(prev_state, prev_action)
+
+        self.weights = utils.combine(1, self.weights, -(step_size * (prediction - target)), features)
+
