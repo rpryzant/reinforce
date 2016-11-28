6389ed77606e6b4e4402b5d9aea570df24f7fc82
rapigan <rapigan@gmail.com>
2016-11-27 18:48:55 -0800

    benchmark clean up

--- test_scripts/benchmark.sh
+++ test_scripts/benchmark.sh
@@ -4,30 +4,30 @@ runs=3
 echo "random baseline...300 x $runs"
 for ((i=1; i<=$runs; i++));
 do
-    echo "\t run $i..."
+    echo " run $i..."
     python main.py -p randomBaseline -b 300 -csv > "random-$i.csv"
 done
-echo "average score (baseline):"
+echo "average cumulative score (baseline):"
 python test_scripts/analyze.py random-1.csv random-2.csv random-3.csv 
 
 	
 echo "q replay...300 x $runs"
 for ((i=1; i<=$runs; i++));
 do
-    echo "\t run $i..."
+    echo " run $i..."
     python main.py -p linearReplayQ -b 300 -csv > "linear-$i.csv"
 done
-echo "average score (q replay):"
+echo "average cumulative score (q replay):"
 python test_scripts/analyze.py linear-1.csv linear-2.csv linear-3.csv 
 
 
 echo "sarsa Lambda...300 x $runs"
 for ((i=1; i<=$runs; i++));
 do
-    echo "\t run $i..."
+    echo " run $i..."
     python main.py -p sarsaLambda -b 300 -csv > "sarsaLambda-$i.csv"
 done
-echo "average score (sarsa lambda):"
+echo "average cumulative score (sarsa lambda):"
 python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3.csv
 
 
@@ -39,7 +39,7 @@ python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3
 #    echo "\t run $i..."
 #    python main.py -p sarsa -b 300 -csv > "sarsaLambda-$i.csv"
 #done
-#echo "average score (sarsa):"
+#echo "average cumulative score (sarsa):"
 #python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3.csv
 
 
@@ -49,7 +49,7 @@ python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3
 #    echo "\t run $i..."
 #    python main.py -p linearQ -b 300 -csv > "sarsaLambda-$i.csv"
 #done
-#echo "average score (q learning):"
+#echo "average cumulative  score (q learning):"
 #python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3.csv
 
 
@@ -59,10 +59,8 @@ python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3
 #    echo "\t run $i..."
 #    python main.py -p nn -b 300 -csv > "sarsaLambda-$i.csv"
 #done
-#echo "average score (nn):"
+#echo "average cumualative score (nn):"
 #python test_scripts/analyze.py sarsaLambda-1.csv sarsaLambda-2.csv sarsaLambda-3.csv
 
-
-	
-
+# clean up...
 rm *.csv
