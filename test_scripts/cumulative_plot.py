5f1150b8f6855ef5b46d7fbef3c8720d65a02be2
rapigan <rapigan@gmail.com>
2016-12-01 06:58:26 -0800

    better multithreading

---
+++ test_scripts/cumulative_plot.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import time
+from tqdm import tqdm
+
+# test configuration
+games = 2000
+runs = 10
+players = ["randomBaseline", "simpleQLearning", "linearQ", "linearReplayQ", "sarsa", "sarsaLambda", "nn", "policyGradients"]
+
+# multithreading configuration
+MAX_PROCESSES = 4
+main_loc = os.path.abspath("main.py")
+command = "python %s -p %s -b %s -csv > %s-%s.csv"
+processes = set()
+
+# throw all commands in a list for tqdm
+commands = [command % (main_loc, player, games, player, i) for player in players for i in range(runs)]
+
+# run em all!
+for cmd in tqdm(commands):
+    processes.add(subprocess.Popen(cmd, shell=True))
+    if len(processes) >= MAX_PROCESSES:
+        os.wait()
+        processes.difference_update([p for p in processes if p.poll() is not None])
+
+
