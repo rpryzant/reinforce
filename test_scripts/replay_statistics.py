4c0d8e2d0dd7c9c10c5eba0f1dfbc91844a5ecd6
rapigan <rapigan@gmail.com>
2016-12-09 07:22:58 -0800

    agh! bug in replay stats script. fixed. will run again tonight

--- test_scripts/replay_statistics.py
+++ test_scripts/replay_statistics.py
@@ -23,11 +23,11 @@ memories = [100, 1000, 5000, 10000]
 samples = [4, 8, 16]
 
 
-train_cmd = "python %s -p linearReplayQ -b %s -e %d -memory_size %d -sample_size %d -wr linearReplayQ-%s.model > linearReplayQ-%s.log"
+train_cmd = "python %s -p linearReplayQ -b %s -e 0.3 -memory_size %d -sample_size %d -wr linearReplayQ-%s-%s-%s.model > linearReplayQ-%s-%s-%s.log"
 
 # start consuming all the training commands concurrently, running MAX_PROCESSES of them at a time
 print "TRAINING..."
-train_commands = [train_cmd % (main_loc, train_games, 0.3, mem_size, sample_size, i, i) for mem_size in memories for sample_size in samples for i in range(1, runs+1)]
+train_commands = [train_cmd % (main_loc, train_games, mem_size, sample_size, i, mem_size, sample_size, i, mem_size, sample_size) for mem_size in memories for sample_size in samples for i in range(1, runs+1)]
 
 for cmd in tqdm(train_commands):
     print '\t' + cmd
@@ -39,7 +39,8 @@ for cmd in tqdm(train_commands):
 
 # then consume all test commands
 print "TESTING..."
-test_commands =  [train_cmd % (main_loc, test_games, 0.01, mem_size, sample_size, i, i) for mem_size in memories for sample_size in samples for i in range(1, runs+1)]
+train_cmd = "python %s -p linearReplayQ -b %s -e 0.01 -memory_size %d -sample_size %d -rd linearReplayQ-%s-%s-%s.model -csv > linearReplayQ-%s-%s-%s.csv"
+test_commands =  [train_cmd % (main_loc, test_games, mem_size, sample_size, i, mem_size, sample_size, i, mem_size, sample_size) for mem_size in memories for sample_size in samples for i in range(1, runs+1)]
 for cmd in tqdm(test_commands):
     print '\t' + cmd
     processes.add(subprocess.Popen(cmd, shell=True))
