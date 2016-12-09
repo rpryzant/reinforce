2d9a7a2b7f4306af143efd0c99d8256b74008493
rapigan <rapigan@gmail.com>
2016-12-09 07:42:00 -0800

    combination script finished. will have to wait until tmoorrow to test

--- test_scripts/combine_csvs.py
+++ test_scripts/combine_csvs.py
@@ -5,17 +5,21 @@ import sys
 
 print sys.argv
 
-
-files = collections.defaultdict(str)
-
-
+# make {run type => [cumulative scores]} mapping
+files = collections.defaultdict(list)
 for file in sys.argv[1:]:
     file_type = '-'.join(file.split('-')[2:])
-    last_line = open(file).readlines()[-1]
-    d[file_type] += last_line + '\n'
+    cumulative_score = open(file).readlines()[-1].split(',')[0]
+    d[file_type].append(cumulative_score)
 
+# make csv with cumulative scores for each run type as columns
+score_matrix = d.items()
+csv = ','.join(name for name, values in score_matrix) + '\n'
+csv += '\n'.join(','.join(values[i] for name, values in score_matrix) for i in range(len(score_matrix[0][1])))
+
+# write the csv
 for filename, csv in d:
-    f = open(filename, 'w')
+    f = open('replayMemory_output', 'w')
     f.write(csv)
     f.close()
 
